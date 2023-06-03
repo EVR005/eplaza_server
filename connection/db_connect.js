d161944f39d9d852aef130fe72f90ac72ab7c4c9
EVR005 <edumbasankaravel513@gmail.com>
2023-06-03 16:55:14 +0530

    shifted to bit.io

--- connection/db_connect.js
+++ connection/db_connect.js
@@ -1,40 +1,12 @@
-// const mysql = require("mysql");
-
-// const connection = mysql.createConnection({
-//   host: "localhost",
-//   user: "raju5",
-//   password: "raju5",
-//   database: "treasure_hunt",
-// });
-
-// const sequelize = () => {
-//   connection.connect(function (err) {
-//     if (err) throw err;
-//     console.log("Connected Database!");
-//   });
-// };
-
-// module.exports = { sequelize };
-
 const Sequelize = require("sequelize");
-// const sequelize = new Sequelize(
-//   "treasure_hunt",
-//   "raju5",
-//   "v2_44JKZ_RZG423xZsLxSCyL2CmjyS6C",
-//   {
-//     host: "localhost",
-//     port: 5432,
-//     dialect: "mysql",
-//     dialectModule: require("mysql2"),
-//   }
-// );
-// const sequelize = new Sequelize(
-//   "postgresql://raju5:v2_44JKZ_RZG423xZsLxSCyL2CmjyS6C@db.bit.io:5432/raju5/treasure_hunt?sslmode=true"
-// );
 
-const sequelize = new Sequelize("eshopping", "raju5", "qWQZ5pF*L0j9S9QD", {
-  host: "localhost",
-  dialect: "mysql",
-  dialectModule: require("mysql2"),
-});
+const sequelize = new Sequelize(
+  "postgresql://raju5:v2_45HBW_85ETi3NNNe2WW2SQqMe2kWC@db.bit.io:5432/raju5/eshopping?sslmode=true"
+);
+
+// const sequelize = new Sequelize("eshopping", "raju5", "qWQZ5pF*L0j9S9QD", {
+//   host: "localhost",
+//   dialect: "mysql",
+//   dialectModule: require("mysql2"),
+// });
 module.exports = { sequelize };
